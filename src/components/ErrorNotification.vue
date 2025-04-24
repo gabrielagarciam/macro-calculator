6c1b34d2d6061215a6d510d260007a667684409a
Gabriela García <gabrielagarciam94@gmail.com>
2025-04-24 16:47:39 -0400

    Update ErrorNotification.vue

--- src/components/ErrorNotification.vue
+++ src/components/ErrorNotification.vue
@@ -3,7 +3,7 @@
       v-if="showModal"
       class="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-60"
     >
-      <div class="bg-white text-black p-6 rounded-lg max-w-md w-full flex flex-col items-center">
+      <div class="bg-white text-black p-6 rounded-lg max-w-xs md:max-w-md w-full flex flex-col items-center">
         <h2 class="text-2xl font-bold text-center mb-4"><span class='text-primary/85'>Oops!</span>Something went wrong</h2>
         <p class="text-center mb-4">
           There was an issue generating the guide. Please try again later.
